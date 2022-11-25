19a941ec0317afe46fcbcb4ddc1b64e2c4db774b
Lucas F. Silva <lucasdotfs@gmail.com>
2022-11-25 17:45:28 -0300

    feat: productsBy pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import LayoutDefault from "./Layouts/LayoutDefault";
 import Cart from "./Pages/Cart/Cart";
 import Login from "./Pages/Login/Login";
 import Product from "./Pages/Product/Product";
+import ProductBySale from "./Pages/ProductBySale/ProductBySale";
+import ProductByType from "./Pages/ProductByType/ProductByType";
 import SignUp from "./Pages/SignUp/SignUp";
 
 export default function App() {
@@ -15,7 +17,8 @@ export default function App() {
             <Route path="/login" exact element={<Login />} />
             <Route path="/cadastro" element={<SignUp />} />
             <Route path="/produto/:productId" element={<Product />} />
-            <Route path="/produtos/:type" element={<></>} />
+            <Route path="/produtos/:type" element={<ProductByType />} />
+            <Route path="/produtos/:type/sale" element={<ProductBySale />} />
             <Route path="/carrinho" element={<Cart />} />
           </Routes>
         </LayoutDefault>
